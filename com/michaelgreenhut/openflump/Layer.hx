9a83d4530ce523cf04c0454db7c439661cf6203f
Michael Greenhut <MJPG777@gmail.com>
2015-05-05 13:46:26 -0400

    Optimized Layer.hx
    Optimized by gigbig@libero.it to handle flipbooks.

--- com/michaelgreenhut/openflump/Layer.hx
+++ com/michaelgreenhut/openflump/Layer.hx
@@ -17,6 +17,7 @@ class Layer
 	private var _index:Int = 0;
 	private var _keyframes:Array<Keyframe>;
 	private var _currentTexture:String;
+	private var _previousTexture:String;
 	private var _currentLocation:Point;
 	private var _currentScale:Point;
 	private var _currentPivot:Point;
@@ -35,6 +36,7 @@ class Layer
 	{
 		_keyframes = new Array<Keyframe>();
 		_preTweenIndex = _index;
+		_image = new Sprite();
 	}
 	
 	public function addKeyframe(kf:Keyframe):Void
@@ -95,73 +97,80 @@ class Layer
 	{
 		if (_index < 0 || _index >= _keyframes.length)
 			return;
-			if (_keyframes[_index].getLocation() != null)
+		if (_keyframes[_index].getLocation() != null)
+		{
+			populateCurrentValues(_index);
+			
+			var textureSprite:Sprite = FlumpTextures.get().getTextureByName(_currentTexture);
+			var originalbm:Bitmap = cast(textureSprite.getChildAt(0), Bitmap);
+			setImage(originalbm.bitmapData.clone());
+			
+			if (_image != null)
 			{
-				populateCurrentValues(_index);
-				if (_image != null)
-				{
-					if (_keyframes[_index].getTweened()) //Stop-gap code to handle tweens
-					{  
+				if (_keyframes[_index].getTweened()) //Stop-gap code to handle tweens
+				{  
+				
+					_destinationIndex = _index + 1;
+					_preTweenIndex = _index;
+					var nextLoc:Point = _keyframes[_destinationIndex].getLocation().clone();  
+					var nextScale:Point = _keyframes[_destinationIndex].getScale().clone();
+					var nextPivot:Point = _keyframes[_destinationIndex].getPivot().clone();
+					var nextAlpha:Float = _keyframes[_destinationIndex].getAlpha();
+					var nextSkew:Point = _keyframes[_destinationIndex].getSkew().clone();
+					_keyframes[_index].internalIndex();
+					var multiplier:Float = _keyframes[_preTweenIndex].internalIndex() /_keyframes[_preTweenIndex].getDuration();
+				
+					_currentAlpha = _keyframes[_preTweenIndex].getAlpha() + (nextAlpha - _keyframes[_preTweenIndex].getAlpha()) * multiplier;
+					_currentScale.x = _keyframes[_preTweenIndex].getScale().x + (nextScale.x - _keyframes[_preTweenIndex].getScale().x) * multiplier;
+					_currentScale.y = _keyframes[_preTweenIndex].getScale().y + (nextScale.y - _keyframes[_preTweenIndex].getScale().y) * multiplier;
+					_currentLocation.x = _keyframes[_index].getLocation().x + (nextLoc.x - _keyframes[_preTweenIndex].getLocation().x) * multiplier;
+				
+					_currentLocation.y = _keyframes[_index].getLocation().y + (nextLoc.y - _keyframes[_preTweenIndex].getLocation().y) * multiplier;
 					
-						_destinationIndex = _index + 1;
-						_preTweenIndex = _index;
-						var nextLoc:Point = _keyframes[_destinationIndex].getLocation().clone();  
-						var nextScale:Point = _keyframes[_destinationIndex].getScale().clone();
-						var nextPivot:Point = _keyframes[_destinationIndex].getPivot().clone();
-						var nextAlpha:Float = _keyframes[_destinationIndex].getAlpha();
-						var nextSkew:Point = _keyframes[_destinationIndex].getSkew().clone();
-						_keyframes[_index].internalIndex();
-						var multiplier:Float = _keyframes[_preTweenIndex].internalIndex() /_keyframes[_preTweenIndex].getDuration();
-					
-						_currentAlpha = _keyframes[_preTweenIndex].getAlpha() + (nextAlpha - _keyframes[_preTweenIndex].getAlpha()) * multiplier;
-						_currentScale.x = _keyframes[_preTweenIndex].getScale().x + (nextScale.x - _keyframes[_preTweenIndex].getScale().x) * multiplier;
-						_currentScale.y = _keyframes[_preTweenIndex].getScale().y + (nextScale.y - _keyframes[_preTweenIndex].getScale().y) * multiplier;
-						_currentLocation.x = _keyframes[_index].getLocation().x + (nextLoc.x - _keyframes[_preTweenIndex].getLocation().x) * multiplier;
-					
-						_currentLocation.y = _keyframes[_index].getLocation().y + (nextLoc.y - _keyframes[_preTweenIndex].getLocation().y) * multiplier;
-						
-						_currentPivot.x = _keyframes[_index].getPivot().x + (nextPivot.x - _keyframes[_preTweenIndex].getPivot().x) * multiplier;
-						_currentPivot.y = _keyframes[_index].getPivot().y + (nextPivot.y - _keyframes[_preTweenIndex].getPivot().y) * multiplier;
-						_currentPivot.x *= _currentScale.x;
-						_currentPivot.y *= _currentScale.y;
-						_currentSkew.x = _keyframes[_index].getSkew().x + (nextSkew.x - _keyframes[_preTweenIndex].getSkew().x) * multiplier;
-						_currentSkew.y = _keyframes[_index].getSkew().y + (nextSkew.y - _keyframes[_preTweenIndex].getSkew().y) * multiplier;
-					
-					}
-					_image.scaleX = _currentScale.x;
-					_image.scaleY = _currentScale.y;
-					
-					_image.x = _currentLocation.x;
-					_image.y = _currentLocation.y;
-					_image.getChildAt(0).x = -_currentPivot.x;
-					_image.getChildAt(0).y = -_currentPivot.y;
-					
-					//if (_currentSkew.x != 0 || _currentSkew.y != 0)
-					{
-						_image.rotation = _currentSkew.x * 180 / Math.PI;
-					}
-					
-					_image.visible = visible;
-					_image.alpha = _currentAlpha;
-					
+					_currentPivot.x = _keyframes[_index].getPivot().x + (nextPivot.x - _keyframes[_preTweenIndex].getPivot().x) * multiplier;
+					_currentPivot.y = _keyframes[_index].getPivot().y + (nextPivot.y - _keyframes[_preTweenIndex].getPivot().y) * multiplier;
+					_currentPivot.x *= _currentScale.x;
+					_currentPivot.y *= _currentScale.y;
+					_currentSkew.x = _keyframes[_index].getSkew().x + (nextSkew.x - _keyframes[_preTweenIndex].getSkew().x) * multiplier;
+					_currentSkew.y = _keyframes[_index].getSkew().y + (nextSkew.y - _keyframes[_preTweenIndex].getSkew().y) * multiplier;
+				
 				}
-				//else 
-				//	trace("NON TWEEN", this.name, "keyframe ", _index);
+				_image.scaleX = _currentScale.x;
+				_image.scaleY = _currentScale.y;
+				
+				_image.x = _currentLocation.x;
+				_image.y = _currentLocation.y;
+				_image.getChildAt(0).x = -_currentPivot.x;
+				_image.getChildAt(0).y = -_currentPivot.y;
+				
+				//if (_currentSkew.x != 0 || _currentSkew.y != 0)
+				{
+					_image.rotation = _currentSkew.x * 180 / Math.PI;
+				}
+				
+				_image.visible = visible;
+				_image.alpha = _currentAlpha;
+				
 			}
-			else 
-			{
-				if (_image != null && _image != {})
-					_image.visible = false; 
-				_currentLocation = null;
-				_currentTexture = null;
-				_currentScale = null;
-				_currentPivot = null;
-			}
+			//else 
+			//	trace("NON TWEEN", this.name, "keyframe ", _index);
+		}
+		else // _keyframes[_index].getLocation() is never == null, so this "else" will never be executed
+		{
+			if (_image != null && _image != {})
+				_image.visible = false; 
+			_currentLocation = null;
+			_currentTexture = null;
+			_currentScale = null;
+			_currentPivot = null;
+		}
 	}
 	
 	private function populateCurrentValues(index:Int):Void 
 	{
+		_previousTexture = _currentTexture;
 		_currentTexture = _keyframes[index].getRef();
+		
 		_currentScale = _keyframes[index].getScale().clone();
 		_currentLocation = _keyframes[index].getLocation().clone();
 		_currentPivot = _keyframes[index].getPivot().clone();
@@ -178,10 +187,17 @@ class Layer
 	
 	public function setImage(bd:BitmapData):Void 
 	{
-		var bm:Bitmap = new Bitmap(bd);
+		/*var bm:Bitmap = new Bitmap(bd);
 		_image = new Sprite();
 		_image.addChild(bm);
-		_originalMatrix = _image.transform.matrix.clone();
+		_originalMatrix = _image.transform.matrix.clone();*/
+		
+		if (_currentTexture != _previousTexture) {
+			var bm:Bitmap = new Bitmap(bd);
+			if (_image.numChildren > 0 && _image.getChildAt(0) != null) (_image.removeChildAt(0));
+			_image.addChild(bm);
+			_originalMatrix = _image.transform.matrix.clone();
+		}
 	}
 	
 	public function setMovie(mv:FlumpMovie)
